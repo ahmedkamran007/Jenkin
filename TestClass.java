68c8a7422199a6cdc0c03789b69981032c152823
ahmedkamran007 <kamranjava@hotmail.com>
2025-05-20 23:55:27 +0200

    initial commit
    first commit

---
+++ TestClass.java
@@ -0,0 +1,13 @@
+	
+public class TestClass {
+
+	public static void main(String[] args) {
+
+		System.out.print("testing 1 in Java program...");
+		System.out.print("testing 2 in java program...");
+
+	
+		
+	}
+
+}
