8624d16aba0dc9502b2d3abc0f5e1e1f98e94f32
ahmedkamran007 <kamranjava@hotmail.com>
2025-05-21 12:29:05 +0200

    init commit

--- TestClass.java
+++ TestClass.java
@@ -3,8 +3,9 @@ public class TestClass {
 
 	public static void main(String[] args) {
 
-		System.out.print("testing 1 in Java program...");
-		System.out.print("testing 2 in java program...");
+		System.out.println("testing 1 in Java program...");
+		System.out.println("testing 2 in java program...");
+			System.out.println("testing 3 in java program...");
 
 	
 		
